877350f3674174922eebf77c6ff4fd68231b6ba8
Sylvain Hugues YOO <sylvain.hugues@yoo.digital>
2025-11-01 20:24:02 +0100

    works

--- src/app/demo-comp.component.ts
+++ src/app/demo-comp.component.ts
@@ -1,15 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { type BooleanInput } from '@angular/cdk/coercion';
+import { booleanAttribute, Component, input } from '@angular/core';
+
 
 @Component({
   selector: 'demo-comp',
   standalone: true,
   template: `
     <div class="demo-comp">
-      Demo Component - isActive: {{ isActive }} | isDisabled: {{ isDisabled }}
+      Demo Component - isActive: {{ isActive() }} | isDisabled: {{ isDisabled() }}
     </div>
   `,
 })
 export class DemoCompComponent {
-  @Input() isActive = false;
-  @Input() isDisabled = false;
+  // @Input() isActive = false;
+  // @Input() isDisabled = false;
+
+
+  isActive = input<boolean, BooleanInput>(true, {
+    transform: booleanAttribute,
+  });
+
+  isDisabled = input<boolean, BooleanInput>(true, {
+    transform: booleanAttribute,
+  });
+
+
 }
